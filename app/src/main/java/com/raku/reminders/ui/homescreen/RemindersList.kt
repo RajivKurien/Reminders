98e8f304fb42846f1cb3e20d01038e730fdcb163
Rajiv.Kurien <rajiv.kurien@zuhlke.com>
2021-06-02 18:07:16 +0100

    Add app title

--- app/src/main/java/com/raku/reminders/ui/homescreen/RemindersList.kt
+++ app/src/main/java/com/raku/reminders/ui/homescreen/RemindersList.kt
@@ -1,13 +1,15 @@
 package com.raku.reminders.ui.homescreen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.raku.reminders.domain.ReminderItem
 import kotlinx.datetime.LocalDate
 
@@ -16,6 +18,16 @@ fun ReminderScreen(
     items: List<ReminderItem>,
 ) {
     Column {
+        Spacer(Modifier.width(12.dp))
+
+        Row(modifier = Modifier.padding(4.dp)) {
+            Spacer(Modifier.width(12.dp))
+            Text(
+                text = "Reminders",
+                fontSize = 25.sp,
+                style = MaterialTheme.typography.h2,
+            )
+        }
         LazyColumn(
             modifier = Modifier.weight(1f),
             contentPadding = PaddingValues(all = 1.dp)
